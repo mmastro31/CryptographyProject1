333b30bd0ae6244f9a4724a4cd1b21ac563043ae
mmastro31 <55255682+mmastro31@users.noreply.github.com>
2021-10-05 14:58:38 -0400

    first commit
    initial files/code

---
+++ frequency_analysis.py
@@ -0,0 +1,24 @@
+
+
+
+
+def createDictionary(text):
+    """
+    create a dictionary of the count of each symbol in given text
+    input: str(either ciphertext or plaintext)
+    output: dict(each symbol matched to frequency)
+    """
+    
+    frequencies = {}
+    
+    #create dictionary of space and a-z
+    frequencies[chr(32)] = 0
+    for asciinumber in range(97,123):
+        frequencies[chr(asciinumber)] = 0
+    
+    #count each letter
+    for letter in text:
+        frequencies[letter] += 1
+
+    return frequencies
+
